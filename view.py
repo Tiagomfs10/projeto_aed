82edb8a299b02407df1767e35a8320d58873686a
Tiagomfs10 <92855450+Tiagomfs10@users.noreply.github.com>
2022-05-24 10:46:45 +0100

    Add files via upload

---
+++ view.py
@@ -0,0 +1,22 @@
+#import controller
+def main():
+    while True:
+        try:
+            input_user = input().split(" ")
+        except EOFError:
+            return
+    
+        #if input_user[0] == RL: #reservar lugar
+            #funcao reservar lugar
+        #if input_user[0] == ER: #eliminar reserva
+            #funcao eliminar reserva
+        #if input_user[0] == AR: #alterar reserva
+            #funcao alterar reserva
+        #if input_user[0] == CVB: #consultar o valor da bilheteira
+            #if input_user[1] == dia:
+                #funcao dia da funcao cvb
+            #if input_user[1] == mes:
+                #funcao mes da funcao cvb
+            #if input_user[1] == ano:
+                #funcao ano da funcao cvb
+        print()
